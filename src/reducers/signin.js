4581584115d99f5e57139d14670886d3bac1dd96
Gustavo Cavalcante <gustavo.cavalcante.oliveira@live.com>
2016-11-10 17:50:44 -0200

    Refactoring code duplication

--- src/reducers/signin.js
+++ src/reducers/signin.js
@@ -10,6 +10,14 @@
 
 import * as types from "../actions";
 
+function stateMap(loading, facebookLoading, error, facebookError, pending){
+	return {loading: loading,
+			facebookLoading: facebookLoading,
+			error: error,
+			facebookError: facebookError,
+			pending: pending};
+}
+
 const signin = (state = {
   loading: false,
   facebookLoading: false,
@@ -18,71 +26,22 @@ const signin = (state = {
 }, action) => {
   switch (action.type) {
   case types.FACEBOOK_SIGNIN_INITIATED:
-    return Object.assign({}, state, {
-      loading: false,
-      facebookLoading: true,
-      error: false,
-      facebookError: false
-    });
+    return Object.assign({}, state, stateMap(false,true,false,false,false));
   case types.FACEBOOK_SIGNIN_SUCCESSFUL:
-    return Object.assign({}, state, {
-      loading: false,
-      facebookLoading: false,
-      error: false,
-      facebookError: false
-    });
+    return Object.assign({}, state, stateMap(false,false,false,false,false));
   case types.FACEBOOK_SIGNIN_FAILED:
-    return Object.assign({}, state, {
-      loading: false,
-      facebookLoading: false,
-      error: false,
-      facebookError: action.errorCode
-    });
+    return Object.assign({}, state, stateMap(false,false,false,action.errorCode,false));
   case types.SIGNIN_INITIATED:
-    return Object.assign({}, state, {
-      loading: false,
-      pending: true,
-      facebookLoading: false,
-      error: false,
-    });
+    return Object.assign({}, state, stateMap(false,false,false,false,true));
   case types.SIGNIN_ERROR:
-    return Object.assign({}, state, {
-      loading: false,
-      pending: false,
-      facebookLoading: false,
-      error: action.data,
-    });
+    return Object.assign({}, state, stateMap(false,false,action.data,false,false));
   case types.CREATEUSER_INITIATED:
-    return Object.assign({}, state, {
-      loading: false,
-      pending: true,
-      facebookLoading: false,
-      error: false,
-    });
+    return Object.assign({}, state, stateMap(false,false,false,false,true));
   case types.CREATEUSER_ERROR:
-    return Object.assign({}, state, {
-      loading: false,
-      pending: false,
-      facebookLoading: false,
-      error: action.data,
-    });
-
-
-
-
-
-
-
+    return Object.assign({}, state, stateMap(false,false,action.data,false,false));
   default:
     return state;
   }
 };
 
 export default signin;
-
-
-
-
-
-
-
